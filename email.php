93b2b6b21895f328d5011d6a17e40664c32b03f6
midiatizou <ti@midiatizou.com.br>
2021-06-15 19:31:01 -0300

    feat: added file email.php

---
+++ email.php
@@ -0,0 +1,23 @@
+<?php
+  
+$name = addcslashes($_POST[name]);
+$email = addcslashes($_POST[email]);
+$message = addcslashes($_POST[message]);
+
+$to = "contato@midiatizou.com.br";
+$subject = "Formulario contato";
+$body = "Nome".$name. "\r\n".
+        "Email".$email. "\r\n".
+        "Mensagem".$message;
+
+$header = "From:ti@midiatizou.com.br"."\r\n".
+          "Replay-To:".$email."\r\n".
+          "X-Mailer:PHP/".phpversion();
+
+if(mail($to, $subject, $body, $header)) {
+  echo("Email enviado com sucesso...");
+} else {
+  echo("Email não pode ser enviado!");
+}
+
+?>
