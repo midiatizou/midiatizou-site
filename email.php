d8f441bb43ef01a17c9c7abd4f3eb1fc4d0317d3
midiatizou <ti@midiatizou.com.br>
2021-06-15 20:04:04 -0300

    feat: added obj.clean() in file php

--- email.php
+++ email.php
@@ -6,15 +6,14 @@ $message = addcslashes($_POST[message]);
 
 $to = "contato@midiatizou.com.br";
 $subject = "Formulario contato";
-$body = "Nome".$name. "\r\n"."Email".$email."\r\n"."Mensagem".$message;
+$body = "Nome"+$name+"\r\n"+"Email"+$email+"\r\n"+"Mensagem"+$message;
 
 ob_clean();
 
-$header = "From:".$email."\r\n"."Replay-To:".$email."\r\n"."X=Mailer:PHP/".phpversion();
+$header = "From:"+$email+"\r\n"+"Replay-To:"+$email+"\r\n"+"X=Mailer:PHP/"+phpversion();
 
 if(mail($to, $subject, $body, $header)) {
   header("Location: https://midiatizou.com.br/obrigado/");
-
 } else {
   echo("Email não pode ser enviado!");
 }
