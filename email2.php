afa4c210da2dee61936531fe2ba222d39aacf9dc
midiatizou <ti@midiatizou.com.br>
2021-06-15 21:04:44 -0300

    feat: added obj.clean() in file php

--- email2.php
+++ email2.php
@@ -1,5 +1,5 @@
 <?php
-header('AMP-Access-Control-Allow-Source-Origin: https://midiatizou.com.br');
+header('AMP-Access-Control-Allow-Source-Origin: https://midia');
 if(isset($_POST['email'])) {
      
     // CHANGE THE TWO LINES BELOW
